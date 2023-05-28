2fdd5b7e3e763e909f7d4a235621574e41d753cb
prithvirajkc <prithviraj1998@hotmail.com>
2023-05-28 19:06:53 +0530

    adding depends_on

--- helm-terraform/grafana.tf
+++ helm-terraform/grafana.tf
@@ -2,7 +2,7 @@ resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
-# version    = "6.15.0"
   namespace = "monitoring"
+  depends_on = [ helm_release.nginx-ingress ]
 }
  
--- helm-terraform/prometheus.tf
+++ helm-terraform/prometheus.tf
@@ -3,4 +3,5 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
   namespace = "monitoring"
+  depends_on = [ helm_release.grafana ]
 }
